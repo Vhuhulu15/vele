1c9f0ac500dc9f1428393218d8f750d1a0d7ff3b
23001673 <160419832+Vhuhulu15@users.noreply.github.com>
2024-04-24 15:48:27 +0200

    Add files via upload

---
+++ Exercise4.COM2123.23001673.cpp
@@ -0,0 +1,266 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+
+ int a;
+ cout<<"Enter your choice(1-5)"<<endl;
+ cin>>a;
+ int b;
+    cout << "Enter integer number 1:"<<endl;
+    cin >> b;
+
+int c;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> c;
+int add=b+c;
+int substract=b-c;
+int multiply=b*c;
+int devide=b/c;
+
+
+
+ if(a==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(a==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(a==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(a==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+ char input;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>input;
+
+
+ if(input=='y' || 'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+ }
+int d;
+ cout<<"Enter your choice(1-5)"<<endl;
+ cin>>d;
+ int e;
+    cout << "Enter integer number 1:"<<endl;
+    cin >> e;
+
+int f;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> f;
+
+ if(d==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(d==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(d==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(d==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+char o;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>o;
+   if(o=='y' || 'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+ }
+int i;
+ cout<<"Enter your choice(1-4)"<<endl;
+ cin>>i;
+ int j;
+    cout << "Enter integer number 1:"<<endl;
+    cin >> j;
+
+int k;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> k;
+
+ if(i==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(i==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(i==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(i==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+char v;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>v;
+  if(v=='y' || 'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+ }
+int l;
+ cout<<"Enter your choice(1-5)"<<endl;
+ cin>>l;
+ int m;
+    cout << "Enter integer number 1:"<<endl;
+    cin >> m;
+
+int n;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> n;
+
+ if(l==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(l==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(l==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(l==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+
+char t;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>t;
+  if(t=='y' || 'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+ }
+int p;
+ cout<<"Enter your choice(1-4)"<<endl;
+ cin>>p;
+ int q;
+    cout << "Enter integer number 1:"<<endl;
+    cin >> q;
+
+int r;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> r;
+
+ if(p==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(p==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(p==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(p==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+
+char w;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>w;
+  if(w=='y' ||'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+ }
+int s;
+ cout<<"Enter your choice(1-4)"<<endl;
+ cin>>s;
+ int y;
+    cout << "Enter integer number 1:"<<endl;
+    cin >>y;
+
+int u;
+    cout << "Enter integer number 2: "<<endl;
+    cin >> u;
+ if(a==1){
+    cout<<"you selected addition"<<endl;
+    cout<<"results:"<<add<<endl;
+ }
+  else if(s==2){
+     cout<<"you selected substraction"<<endl;
+    cout<<"results:"<<substract<<endl;
+   }
+    else if(s==3){
+     cout<<"you selected multiplication"<<endl;
+    cout<<"results:"<<multiply<<endl;
+   }
+  else if(s==4){
+     cout<<"you selected devision"<<endl;
+    cout<<"results:"<<devide<<endl;
+   }
+
+
+char z;
+cout<<"enter y or Y to continue:"<<endl;
+cin>>z;
+  if(z=='y' || 'Y'){
+     cout << "MENU"<<endl;
+cout<<"1.Add"<<endl;
+cout<<"2.substract"<<endl;
+cout<<"3.multiply"<<endl;
+cout<<"4.Divide"<<endl;
+
+
+ }
+
+
+
+    return 0;
+}
